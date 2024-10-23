cece12c7c83307ce170faa0172758b2ec34ecbd6
GigiKitty <alelysainz938@gmail.com>
2024-10-23 09:32:13 -0700

    cambie el color de fondo

--- Form1.cs
+++ Form1.cs
@@ -43,5 +43,20 @@ namespace proyectosemanastf
 		{
 
 		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+
+		}
+
+		private void textBox2_TextChanged(object sender, EventArgs e)
+		{
+			
+		}
+
+		private void textBox1_TextChanged(object sender, EventArgs e)
+		{
+
+		}
 	}
 }
